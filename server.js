2b688d3b66ebadaec32309c50681255563419cf6
Ayodele Temitope <dyenamics228@gmail.com>
2025-01-01 16:39:47 +0100

    add route to get users

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
-import cookieparser from "cookie-parser";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 import connectDb from "./config/db.js";
 import auth from "./routes/auth.js";
 import userRoute from "./routes/userRoutes.js";
@@ -11,7 +12,18 @@ connectDb();
 const app = express();
 
 // middleware
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+const corsOptions = {
+  origin: "http://localhost:5173", // Allow your frontend origin
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true, // Allow cookies and authentication headers
+};
+app.use(cors(corsOptions));
+app.options("*", cors());
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
