e646828ef343ba938349766156b5933fac89f3aa
Ayodele Temitope <dyenamics228@gmail.com>
2025-01-17 19:56:06 +0100

    include localhost as origin

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const corsOptions = {
-  origin: "https://life-trak.vercel.app",
+  origin: ["https://life-trak.vercel.app", "http://localhost:5173"],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
