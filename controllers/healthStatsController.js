e8dc7354fdc4309900e19a95e286e16f549826a5
Ayodele Temitope <dyenamics228@gmail.com>
2025-01-18 15:43:34 +0100

    fix1

--- controllers/healthStatsController.js
+++ controllers/healthStatsController.js
@@ -52,6 +52,24 @@ export const createHealthStats = async (req, res) => {
   }
 };
 
+// Fetch User with populated healthStats
+export const getUserWithHealthStats = async (req, res) => {
+  try {
+    const userId = req.params.userId;
+
+    // Find the user by their ID and populate the healthStats field
+    const user = await User.findById(userId).populate("healthStats");
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.status(200).json(user); // Send the user with full healthStats populated
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
 // Get All Health Stats for the Logged-in User
 export const getAllHealthStats = async (req, res) => {
   try {
