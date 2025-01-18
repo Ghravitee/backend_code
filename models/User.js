a58411c97720ac847d50fb5dc09e372284bf3f4d
Ayodele Temitope <dyenamics228@gmail.com>
2025-01-18 14:34:45 +0100

    add healthSchema again

--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 import bcrypt from "bcrypt";
-import healthStatsSchema from "../models/HealthStats";
+import healthStatsSchema from "../models/HealthStats.js";
 
 const userSchema = new mongoose.Schema(
   {
